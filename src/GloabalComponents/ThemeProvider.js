955b27aca057c2201290dc964404603b6629a6fd
Anna Baharyan <anna.baharyan91@gmail.com>
2022-10-08 09:17:21 +0400

    first commit

---
+++ src/GloabalComponents/ThemeProvider.js
@@ -0,0 +1,24 @@
+import React,{useState,createContext,useContext,useEffect} from 'react'
+
+
+const ThemeContext=createContext()
+
+const ThemeProvider = (props) => {
+    const [theme,setTheme]=useState(JSON.parse(localStorage.getItem('theme'))||false)
+    useEffect(()=>{
+       localStorage.setItem('theme',JSON.stringify(theme))
+    },[theme])
+
+    const setThemeMode=mode=>setTheme(mode)
+  return (
+   <ThemeContext.Provider value={{theme,setThemeMode}}>
+      {props.children}
+
+   </ThemeContext.Provider>
+  )
+}
+const useThemeHook=()=>{
+    const {theme}=useContext(ThemeContext)
+    return [theme]
+}
+export {ThemeProvider,ThemeContext,useThemeHook} 
---
+++ src/Pages/Cart.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { Button, Container, Col, Row, Table } from 'react-bootstrap'
+import { useCart } from 'react-use-cart';
+import { useThemeHook } from '../GloabalComponents/ThemeProvider';
+import { BsCartCheck, BsCartX } from 'react-icons/bs'
+const Cart = () => {
+    const [theme] = useThemeHook()
+    const {
+        isEmpty,
+        items,
+        cartTotal,
+        updateItemQuantity,
+        removeItem,
+        emptyCart,
+    } = useCart()
+    return (
+        <Container className='py-4 mt-5'>
+            <h1 className={`${theme ? 'text-light' : 'text-light-primary'} my-5 text-center`}>
+                {isEmpty ? 'Your  Cart is Empty' : 'The Cart'}
+                <Row className='justify-content-center'>
+                    <Table responsive='sm' striped bordered hover variant={theme ? "dark" : 'light'} className='mb-5'>
+                        <tbody>
+                            {items.map((item, index) => (
+                                <tr key={index} >
+                                    <td>
+                                        <div style={{
+                                            background: 'white', height: '8rem',
+                                            overflow: 'hidden', display: 'flex',
+                                            justifyContent: 'center', alignItems: 'center',
+                                        }}>
+                                            <div style={{ padding: '0.5rem' }}>
+                                                <img src={item.image} style={{ width: '4rem' }} alt={item.title} />
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <h6 style={{ whiteSpace: 'nowrap', width: '14rem', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                                            {item.title}
+                                        </h6>
+                                    </td>
+                                    <td>
+                                        <h6 style={{ whiteSpace: 'nowrap', width: '14rem', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                                            Rs. {item.price}
+                                        </h6>
+                                    </td>
+                                    <td>
+                                        <h6 style={{ whiteSpace: 'nowrap', width: '14rem', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                                            Quantity({item.quantity})
+                                        </h6>
+                                    </td>
+                                    <td>
+                                        <Button className='ms-2'
+                                        onClick={()=>updateItemQuantity(item.id, item.quantity-1)}
+                                        >-</Button>
+                                        <Button className='ms-2'
+                                        onClick={()=>updateItemQuantity(item.id, item.quantity+1)}
+                                        >+</Button>
+                                        <Button variant='danger'className='ms-2'
+                                        onClick={()=>removeItem(item.id, item.quantity+1)}
+                                        >Remove</Button>
+                                    </td>
+                                </tr>
+                            ))}
+
+
+                        </tbody>
+                    </Table>
+                    {!isEmpty&&
+                    <Row style={{position:'fixed', bottom:0}}
+                    className={`${theme?'bg-light-black text-light':'bg-light text-black'} justufy-content-center w-100`}
+                    >
+                      <Col className='py-2'>
+                       <h4>Total price: Rs. {cartTotal} </h4>
+                      </Col>
+                      <Col className='p-0' md={4}>
+                        <Button variant='danger' className='m-2'
+                        onClick={()=>emptyCart()}>
+                            <BsCartX size='1.7rem'/>
+                            Clear Card
+                        </Button>
+                        <Button variant='success' className='m-2'
+                        onClick={()=>emptyCart()}>
+                            <BsCartCheck size='1.7rem'/>
+                            Clear Card
+                        </Button>
+                      </Col>
+                    </Row>
+
+                    
+                    }
+                </Row>
+            </h1>
+        </Container>
+    );
+}
+
+export default Cart;
